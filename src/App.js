1813ab0fa4167f2dd2a51c3dff59ca1e397d8254
sudddy <suddpro@gmail.com>
2020-11-17 00:23:33 +1300

    creating jumbotron component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
 import React from "react";
+import jumboData from "./fixtures/jumbo";
+import Jumbotron from "./components/jumbotron/index";
+import GlobalStyles from "./global-styles";
 
 export default function App() {
-  return <p> hello </p>;
+  console.log(jumboData);
+  return (
+    <Jumbotron.Container>
+      {jumboData.map(item => (
+        <Jumbotron key={item.id} direction={item.direction}>
+          <Jumbotron.Pane>
+            <Jumbotron.Title>{item.title}</Jumbotron.Title>
+            <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
+          </Jumbotron.Pane>
+          <Jumbotron.Pane>
+            <Jumbotron.Image src={item.image}></Jumbotron.Image>
+          </Jumbotron.Pane>
+        </Jumbotron>
+      ))}
+    </Jumbotron.Container>
+  );
 }
