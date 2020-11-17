4995d2ec0292749c26e4a88332997dcd4805c7e8
sudddy <suddpro@gmail.com>
2020-11-17 13:34:12 +1300

    container and wrapping up jumbotron

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
 import React from "react";
 import jumboData from "./fixtures/jumbo";
-import Jumbotron from "./components/jumbotron/index";
-import GlobalStyles from "./global-styles";
+import { JumbotronContainer } from "./containers/jumbotron";
 
 export default function App() {
   console.log(jumboData);
-  return (
-    <Jumbotron.Container>
-      {jumboData.map(item => (
-        <Jumbotron key={item.id} direction={item.direction}>
-          <Jumbotron.Pane>
-            <Jumbotron.Title>{item.title}</Jumbotron.Title>
-            <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
-          </Jumbotron.Pane>
-          <Jumbotron.Pane>
-            <Jumbotron.Image src={item.image}></Jumbotron.Image>
-          </Jumbotron.Pane>
-        </Jumbotron>
-      ))}
-    </Jumbotron.Container>
-  );
+  return <JumbotronContainer />;
 }
